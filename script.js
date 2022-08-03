78abb10719875925086d511a4fcf935b710814ad
Carlos Eduardo <eduardo-1942@hotmail.com>
2022-08-03 20:49:11 -0300

    aula 012

---
+++ script.js
@@ -0,0 +1,47 @@
+// const bool1 = false
+// const bool2 = true
+
+// if(bool1) {
+//     alert("Entrei no if, sou true")
+// } else {
+//     alert("entrei no else, sou false")
+// }
+
+// const bool1 = false
+// const bool2 = true
+// const bool3 = true
+
+// if(bool1 === bool2) {
+//     alert("1 e 2 são iguais")
+// } else if (bool2 === bool3){
+//     alert("2 e 3 são iguais!")
+// } else if(bool1 === bool3) {
+//     alert("1 e 3 São Iguais!")
+// } else {
+//     alert("Nenhum das condilões foi atendida!!")
+// }
+// const idade = 19
+ 
+// if(idade>= 60) {
+//     alert("O voto é facultativo")
+// } else if(idade >= 16 && idade < 18){
+//     alert("O voto é facultativo")
+// }
+// else if(idade >= 18) {
+//     alert("Pode e Deve emitir o titulo")
+// } else {
+//     alert("Não pode emitir o titulo")
+// }
+
+// exercicio de fixação
+
+const media = 3
+if(media>= 5) {
+    alert("Você foi aprovado!")
+}else if(media >= 3) {
+    alert("Você está de Recuperação")
+}else if(media< 3) {
+    alert("Você foi reprovado.")
+}else {
+    alert("Dado inválido")
+}
